34408e218fe41585792ed9af06c88d6377eb033b
frederikmahipal <frederikmhv@gmail.com>
2024-10-20 13:58:46 +0200

    revert to working state :D

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,21 @@
-<<<<<<< HEAD
-import { getProfileService, updateProfileService, searchUsersService, getUserInfoService } from '../services/userService.js';
-=======
 import { getProfileService, updateProfileService, searchUsersService, getOtherUserProfileService } from '../services/userService.js';
->>>>>>> recovery-branch
 
-// Controller to get current user profile
+// Controller to get user profile
 export const getProfile = async (req, res) => {
   try {
-    const userId = req.user._id; 
-    console.log(userId);
+    const userId = req.user.id; // Assuming user ID is available in the request object
+    console.log("USER ID ", userId);
     const userProfile = await getProfileService(userId);
-    console.log(userProfile);
     res.status(200).json(userProfile);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching user profile', error });
   }
 };
 
-// Controller to update current users profile
+// Controller to update user profile
 export const updateProfile = async (req, res) => {
   try {
-    const userId = req.user._id; // Assuming user ID is available in the request object
+    const userId = req.user.id; // Assuming user ID is available in the request object
     const updatedProfile = await updateProfileService(userId, req.body);
     res.status(200).json(updatedProfile);
   } catch (error) {
@@ -28,30 +23,6 @@ export const updateProfile = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-
-// Controller to search for users
-export const searchUsers = async (req, res) => {
-  try {
-    const query = req.query.q; // Get the search query from the request query parameters
-    const currentUserId = req.user._id; // Get the current user's ID to exclude it from results
-    const users = await searchUsersService(query, currentUserId);
-    res.status(200).json(users);
-  } catch (error) {
-    console.error('Error searching for users:', error); 
-    res.status(500).json({ message: 'Error searching for users', error: error.message });
-  }
-};
-
-// Controller to get user details
-export const getUserInfo = async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const user = await getUserInfoService(userId);
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching user details', error });
-=======
 // Controller to search users
 export const searchUsers = async (req, res) => {
   try {
@@ -73,6 +44,5 @@ export const getOtherUserProfile = async (req, res) => {
     res.status(200).json(userProfile);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching user profile', error });
->>>>>>> recovery-branch
   }
 };
