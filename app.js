34638db312b1d05cc711119729c39b008934c2a7
frederikmahipal <frederikmhv@gmail.com>
2025-01-05 23:24:52 +0100

    added cors to vercel.json

--- app.js
+++ app.js
@@ -25,14 +25,8 @@ const corsOptions = {
         'https://client-7xd81ltpe-frederiks-projects-6d4123e7.vercel.app',
         'http://localhost:5173'
     ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    exposedHeaders: ['set-cookie']
+    credentials: true
 };
-
-// Enable pre-flight requests for all routes
-app.options('*', cors(corsOptions));
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
